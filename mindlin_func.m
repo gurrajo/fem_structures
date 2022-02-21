36dc464215d0b6a254dcfebe436ddaf5d228e9f7
gurrajo <77624619+gurrajo@users.noreply.github.com>
2022-02-21 19:44:32 +0100

    Update mindlin_func.m

--- mindlin_func.m
+++ mindlin_func.m
@@ -1,4 +1,5 @@
-
+a = sym('a',[20,1],'real');
+z = sym('z',[1,1],'real');
 xi = sym('xi',[2,1],'real');
 N1 = 0.25*(xi(1) - 1)*(xi(2) - 1); N2 = -0.25*(xi(1) + 1)*(xi(2) - 1);
 N3 = 0.25*(xi(1) +1)*(xi(2) + 1); N4 = -0.25*(xi(1) - 1)*(xi(2) + 1);
@@ -39,13 +40,13 @@ G = sym('G',[2,2],'real');
 h = sym('h',[1,1],'real');
 D_0 = D*h;
 G_0 = G*h;
-D_tilde = h^3/12*D;
+D_tilde = ((h^3)/12)*D;
 K_uu = h*detFisop*Be_1'*D_0*Be_1;
 K_ww = h*detFisop*Be_2'*G_0*Be_2;
-K_wtheta = h*detFisop*Be_2'*G_0*Ne;
+K_wtheta = -h*detFisop*Be_2'*G_0*Ne;
 K_thetaw = K_wtheta';
 K_thetaB = h*detFisop*Be_1'*D_tilde*Be_1;
-K_thetaS =h*detFisop*Ne'*G_0*Ne;
+K_thetaS = h*detFisop*Ne'*G_0*Ne;
 Ke_1 = sym(zeros(20,20));
 Ke_1(1:8,1:8) = K_uu;
 Ke_1(13:20,13:20) = K_thetaB;
@@ -57,5 +58,14 @@ Ke_2(9:12,13:20) = K_wtheta;
 Ke_2(13:20,9:12) = K_thetaw;
 Ke_2(13:20,13:20) = K_thetaS;
 
+a_u = a([1,2,6,7,11,12,16,17]);
+a_w = a(3:5:end);
+a_theta = a([5,4,10,9,15,14,20,19]);
+
+sigma = D*(Be_1*a_u - z*Be_1*a_theta);
+
+tau = G*(Be_2*a_w-Ne*a_theta);
+
 matlabFunction(Ke_1,'File','Ke_mindlin_func_1','Vars',{xi,xe1,xe2,xe3,xe4,D,G,h});
 matlabFunction(Ke_2, detFisop,'File','Ke_mindlin_func_2','Vars',{xi,xe1,xe2,xe3,xe4,D,G,h});
+matlabFunction(sigma, tau,'File','Stress_mindlin_func_2','Vars',{xi,xe1,xe2,xe3,xe4,a,D,G,z});
