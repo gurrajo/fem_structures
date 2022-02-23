af95ffee122b4d5cd3c4223218131f98b6cdfc81
gurrajo <77624619+gurrajo@users.noreply.github.com>
2022-02-23 21:32:58 +0100

    Update fem_struc_assig_2.m

--- fem_struc_assig_2.m
+++ fem_struc_assig_2.m
@@ -3,8 +3,8 @@ xmin = 0;
 xmax = 10;
 ymin = 0;
 ymax = 2.5;
-nelx = 200;
-nely = 15;
+nelx = 40; % x00 makes sure a node is placed on either end of traction boundary
+nely = 10;
 [mesh, coord, Edof] = rectMesh(xmin, xmax, ymin, ymax, nelx, nely); % el, u1, v1, w1, thetay1, thetax1,
 E = 80*10^9;
 v = 0.2;
@@ -17,6 +17,7 @@ D = hooke(1,E,v);
 G = E/(2*(1+v))*[1 , 0; 0 , 1];
 rho = 1000;
 g = 9.81;
+
 total_t = -p_weight*g/l;
 
 
@@ -32,7 +33,7 @@ end
 dof_F = [1:ndofs];
 dof_C = [];
 for i = 1:length(node_C)
-    dof_C_node = [node_C(i)*5-4 node_C(i)*5-3 node_C(i)*5-2 node_C(i)*5-1 node_C(i)*5]; % wrong order
+    dof_C_node = [node_C(i)*5-4 node_C(i)*5-3 node_C(i)*5-2 node_C(i)*5-1 node_C(i)*5]; 
     dof_C = horzcat(dof_C,dof_C_node);
 end
 a_C = zeros(1,length(dof_C))';
@@ -43,24 +44,27 @@ f = zeros(ndofs,1);
 xi_v = [-1/sqrt(3) -1/sqrt(3) 1/sqrt(3) 1/sqrt(3);
         -1/sqrt(3) 1/sqrt(3) -1/sqrt(3) 1/sqrt(3)];
 xi_2 = [0,0]';
-pres_v = zeros(1,20);
-pres_v(3:5:end) = 1;
 Ex = zeros(nel,4);
 Ey = zeros(nel,4);
 for el = 1:nel
+   
     fe = zeros(1,20);
     nodes = mesh(:,el);
     Ex(el,:) = coord(nodes,1);
     Ey(el,:) = coord(nodes,2); 
     y_middle = sum(coord(nodes,2))/4; 
-    P = rho*g*(ymax - y_middle);
+
+    %% Stiffness matrix
     Ke_1 = zeros(20,20);
     for i = 1:4
         xi = xi_v(:,i);
         Ke_1 = Ke_1 + Ke_mindlin_func_1(xi,coord(nodes(1),:)',coord(nodes(2),:)',coord(nodes(3),:)',coord(nodes(4),:)',D,G,h);
     end
-    [Ke_2,detFisop] = Ke_mindlin_func_2(xi_2,coord(nodes(1),:)',coord(nodes(2),:)',coord(nodes(3),:)',coord(nodes(4),:)',D,G,h);
-    Ke = Ke_1 + Ke_2*2;
+    Ke_2 = Ke_mindlin_func_2(xi_2,coord(nodes(1),:)',coord(nodes(2),:)',coord(nodes(3),:)',coord(nodes(4),:)',D,G,h);
+    w_ir1 = 4; % weight ir = 1
+    Ke = Ke_1 + Ke_2*w_ir1;
+
+    %% Applied traction
     for i = 1:3
         x = coord(nodes(i),1);
         y = coord(nodes(i),2);
@@ -72,16 +76,24 @@ for el = 1:nel
                     del_x = abs(coord(nodes(i),1)-coord(nodes(j),1));
                     del_t = total_t*del_x;
                     top_trac = zeros(1,20);
-                    top_trac(i*5-3) = del_t/2;
+                    top_trac(i*5-3) = del_t/2; % applied in y dof for nodes on traction boundary
                     top_trac(j*5-3) = del_t/2;
-                    fe = fe + top_trac;
+                    %fe = fe + top_trac;
                     break;
                 end
             end
         end
     end
-    fe = fe + detFisop*P/4*pres_v;
+    %% Pressure
+    P = -rho*g*(ymax - y_middle);
+    for i = 1:4
+        xi = xi_v(:,i);
+        fe(3:5:end) = fe(3:5:end) + fe_press_mindlin_func_1(xi,coord(nodes(1),:)',coord(nodes(2),:)',coord(nodes(3),:)',coord(nodes(4),:)',P)';
+    end
+
+    %% Assemble
     K(Edof(el,2:end),Edof(el,2:end)) = K(Edof(el,2:end),Edof(el,2:end)) + Ke;
+
     f(Edof(el,2:end))= f(Edof(el,2:end)) + fe';
 end
 
@@ -100,6 +112,7 @@ plotpar=[1 1 0];
 sfac = 1e3; % magnification factor
 eldisp2(Ex,Ey,Ed_xy,plotpar,sfac);
 
+%% stress calculations
 z = 0;
 for el = 1:nel
     nodes = mesh(:,el);
@@ -108,8 +121,8 @@ for el = 1:nel
     sigma_vm(el) = sqrt(3/2*(S'*S + sigma(el,3)^2 + tau(el,1)^2 + tau(el,2)^2));
 end
 figure
-plotpar=[1 1 0];
-sfac = 1e2; % magnification factor
+plotpar = [1 1 0];
+sfac = 1e3; % magnification factor
 eldisp2_fill(Ex,Ey,Ed_xy,plotpar,sfac,sigma_vm);
 hold on
 xlabel ("[m]")
@@ -117,26 +130,60 @@ ylabel ("[m]")
 title("Deformed geometry for full top side displacement")
 colorbar
 
-% xi_3x3 = []
-% 
-% for el = 1:nel
-%     nodes = mesh(:,el);
-%     Ex(el,:) = coord(nodes,1);
-%     Ey(el,:) = coord(nodes,2); 
-%     Ke_ww = zeros(12,12);
-%     for i = 1:4
-%         xi = xi_v(:,i);
-%         Ke_ww = Ke_ww + Kww_kirchoff_func(xi,coord(nodes(1),:)',coord(nodes(2),:)',coord(nodes(3),:)',coord(nodes(4),:)',D,G,h);
-%     end
-%     for i = 1:9
-%         xi = xi_3x3(:,i);
-%         Ge = Ge + Ge_kirchoff_func(xi,coord(nodes(1),:)',coord(nodes(2),:)',coord(nodes(3),:)',coord(nodes(4),:)',D,G,h);
-%     end
-%     
-%     fe = fe + detFisop*P/4*pres_v;
-%     K(Edof(el,2:end),Edof(el,2:end)) = K(Edof(el,2:end),Edof(el,2:end)) + Ke;
-%     f(Edof(el,2:end))= f(Edof(el,2:end)) + fe';
-% end
+figure 
+w = Ed(:,3:5:end);
+fill(Ex',Ey',w');
+hold on
+xlabel ("[m]")
+ylabel ("[m]")
+title("z deflection")
+colorbar
 
+%% Task 2
+gp = [-sqrt(3/5), 0, sqrt(3/5)];
+wi = [5/9, 8/9];
+xi_3x3 = [gp(1), gp(2), gp(3), gp(1), gp(2), gp(3), gp(1), gp(2), gp(3);
+          gp(1), gp(1), gp(1), gp(2), gp(2), gp(2), gp(3), gp(3), gp(3)];
+wi_3x3 = [wi(1), wi(2), wi(1), wi(1), wi(2), wi(1), wi(1), wi(2), wi(1);
+          wi(1), wi(1), wi(1), wi(2), wi(2), wi(2), wi(1), wi(1), wi(1)];
+Gr = spalloc(nnodes,nnodes,5*ndofs);
+Kww = spalloc(nnodes*3,nnodes*3,5*ndofs);
+for el = 1:nel
+    Edof_G(el,1) = el;
+    Edof_Kww(el,1) = el;
+    for i = 1:4
+        Edof_G(el,i+1) = (Edof(el,(i*5-3)) + 4)/5;
+    end
+    j = 2;
+    for i = 1:4
+        node = (Edof(el,(i*5-3)) + 4)/5; 
+        Edof_Kww(el,j) = node*3-2;
+        j = j+1;
+        Edof_Kww(el,j) = node*3-1;
+        j = j+1;
+        Edof_Kww(el,j) = node*3;
+        j = j+1;
+    end
+    nodes = mesh(:,el);
+    Ex(el,:) = coord(nodes,1);
+    Ey(el,:) = coord(nodes,2); 
+    Ke_ww = zeros(12,12);
+    Ge = zeros(4,4);
+    N_tilde = h*[sigma(el,1) sigma(el,3);sigma(el,3) sigma(el,2)];
+    for i = 1:4
+        % w = 1 
+        xi = xi_v(:,i);
+        Ke_ww = Ke_ww + Kww_kirchoff_func(xi,coord(nodes(1),:)',coord(nodes(2),:)',coord(nodes(3),:)',coord(nodes(4),:)',D,h);
+    end
+    for i = 1:9
+        xi = xi_3x3(:,i);
+        w = wi_3x3(1,i)*wi_3x3(2,i);
+        Ge = Ge + w*Ge_kirchoff_func(xi,coord(nodes(1),:)',coord(nodes(2),:)',coord(nodes(3),:)',coord(nodes(4),:)',N_tilde,h);
+    end
+    Kww(Edof_Kww(el,2:end),Edof_Kww(el,2:end)) = Kww(Edof_Kww(el,2:end),Edof_Kww(el,2:end)) + Ke_ww;
+    Gr(Edof_G(el,2:end),Edof_G(el,2:end)) = Gr(Edof_G(el,2:end),Edof_G(el,2:end)) + Ge;
+end
+n_lambda = 3;
+[V,D] = eigs(Kww(dof_F,dof_F),-Gr(dof_F,dof_F),n_lambda,'smallestabs');
 
 
